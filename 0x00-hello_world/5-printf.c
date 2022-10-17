c816fee22beadf740afb35c122cbb248883bbbc9
Bachaendale <bacha.efoye@gmail.com>
2022-10-17 11:13:11 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print a c program using printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with a proper grammar, but the out come is a piece of art,\n")
+	return (0);
+}
